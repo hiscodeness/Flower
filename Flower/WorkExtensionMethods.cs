9e3f9c49e732917c490afa396a83fe0e0a7483e0
Jussi Mattila <jussi.mattila@combinostics.com>
2014-11-12 22:15:04 +0200

    Remove piping of action works/workers.
    Clients should simply reuse the trigger.

--- Flower/WorkExtensionMethods.cs
+++ Flower/WorkExtensionMethods.cs
@@ -6,18 +6,6 @@ namespace Flower
 {
     public static class WorkExtensionMethods
     {
-        public static IActionWork<TInput> Pipe<TInput>(
-              this IActionWork<TInput> work, IWorkerResolver<TInput> workerResolver)
-        {
-            return work.Registration.WorkRegistry.Register(work.Executed.Select(w => w.Input), workerResolver);
-        }
-
-        public static IActionWork<TInput> Pipe<TInput>(
-            this IActionWork<TInput> work, IWorker<TInput> worker)
-        {
-            return work.Registration.WorkRegistry.Register(work.Executed.Select(w => w.Input), worker);
-        }
-
         public static IFuncWork<TInput, TOutput> Pipe<TPrevInput, TInput, TOutput>(
             this IFuncWork<TPrevInput, TInput> work, IWorkerResolver<TInput, TOutput> workerResolver)
         {
