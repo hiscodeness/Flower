25143963c14945fb66ad7aa44a6b57ce4b073121
Jussi Mattila <jussi.mattila@combinostics.com>
2014-11-18 10:03:22 +0200

    Add background thread work runner.

---
+++ Flower.Tests/WorkRunners/BackgroundThreadQueueWorkRunnerTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FakeItEasy;
+using Flower.WorkRunners;
+using Flower.Works;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Flower.Tests.WorkRunners
+{
+    public class BackgroundThreadQueueWorkRunnerTests
+    {
+        [Theory]
+        [InlineData(5, 100)]
+        [InlineData(100, 5)]
+        public void BackgroundTreadQueueExecutesOneWorkWhileOthersArePending(int delayInMilliseconds, int workCount)
+        {
+            // Arrange
+            var countdown = new CountdownEvent(workCount);
+            var workRunner = new BackgroundThreadQueueWorkRunner();
+            var workRunnerSnapshots = new List<WorkRunnerSnapshot>();
+            var executableWork = A.Fake<IExecutableWork>();
+            A.CallTo(() => executableWork.Execute()).Invokes(_ =>
+                {
+                    Task.Delay(delayInMilliseconds).Wait();
+                    workRunnerSnapshots.Add(GetSnapshot(workRunner));
+                    countdown.Signal();
+                });
+
+            // Act
+            for (var i = 0; i < workCount; i++)
+            {
+                workRunner.Submit(executableWork);
+            }
+
+            // Assert
+            countdown.Wait(TimeSpan.FromSeconds(10));
+            Assert.Equal(workCount, workRunnerSnapshots.Count);
+            Assert.True(workRunnerSnapshots.Select(state => state.ExecutingWorks.Count).All(count => count == 1));
+            var maxPendingCount = Enumerable.Range(1, workCount-1).Aggregate((acc, x) => acc + x);
+            Assert.InRange(workRunnerSnapshots.Sum(state => state.PendingWorks.Count), workCount, maxPendingCount);
+        }
+        
+        private static WorkRunnerSnapshot GetSnapshot(IWorkRunner workRunner)
+        {
+            return 
+                new WorkRunnerSnapshot
+                    {
+                        PendingWorks = workRunner.PendingWorks.ToList(),
+                        ExecutingWorks = workRunner.ExecutingWorks.ToList()
+                    };
+        }
+
+        private struct WorkRunnerSnapshot
+        {
+            public List<IExecutableWork> PendingWorks { get; set; }
+            public List<IExecutableWork> ExecutingWorks { get; set; }
+        }
+    }
+}
---
+++ Flower/WorkRunners/BackgroundThreadQueueWorkRunner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flower.Works;
+
+namespace Flower.WorkRunners
+{
+    public class BackgroundThreadQueueWorkRunner : IWorkRunner
+    {
+        private readonly BlockingCollection<IExecutableWork> pendingWorks = new BlockingCollection<IExecutableWork>();
+        private readonly BlockingCollection<IExecutableWork> executingWorks = new BlockingCollection<IExecutableWork>();
+
+        public BackgroundThreadQueueWorkRunner()
+        {
+            // All tasks are queued and dequeued on a separate long running thread
+            Task.Factory.StartNew(DequeuePendingWorksThread, TaskCreationOptions.LongRunning);
+        }
+
+        public IEnumerable<IExecutableWork> PendingWorks { get { return pendingWorks; } }
+        public IEnumerable<IExecutableWork> ExecutingWorks { get { return executingWorks; } }
+        
+        public void Submit(IExecutableWork executableWork)
+        {
+            pendingWorks.Add(executableWork);
+        }
+
+        private void DequeuePendingWorksThread()
+        {
+            foreach (var work in pendingWorks.GetConsumingEnumerable())
+            {
+                executingWorks.Add(work);
+                work.Execute();
+                executingWorks.Take();
+            }
+
+            pendingWorks.Dispose();
+        }
+    }
+}
