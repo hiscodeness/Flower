f3978f8ac240068cc19cca0c7fa085f530e45119
Jussi Mattila <jussi.mattila@combinostics.com>
2014-11-19 09:22:18 +0200

    Add support to create Autofac owned instances for worker execution.

---
+++ Flower.Autofac/CreateOwnedInstanceExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Autofac.Features.OwnedInstances;
+using Flower.Autofac;
+
+namespace Flower
+{
+    public static class CreateOwnedInstanceExtensionMethods
+    {
+        public static Func<IScope<T>> Scope<T>(this Func<Owned<T>> resolve)
+        {
+            return () => new OwnedScope<T>(resolve);
+        }
+    }
+}
---
+++ Flower.Autofac/OwnedScope.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Autofac.Features.OwnedInstances;
+
+namespace Flower.Autofac
+{
+    public class OwnedScope<T> : IScope<T>
+    {
+        private readonly Owned<T> owned;
+
+        public OwnedScope(Func<Owned<T>> createOwned)
+        {
+            owned = createOwned();
+            Worker = owned.Value;
+        }
+
+        public T Worker { get; private set; }
+
+        public void Dispose()
+        {
+            owned.Dispose();
+        }
+    }
+}
