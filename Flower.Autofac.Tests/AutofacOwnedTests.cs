488c90ef14702679137875d5fc1dc1444b9fc47e
Jussi Mattila <jussi.mattila@combinostics.com>
2014-11-21 09:46:34 +0200

    Add test for piping owned works.

--- Flower.Autofac.Tests/AutofacOwnedTests.cs
+++ Flower.Autofac.Tests/AutofacOwnedTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Reactive.Subjects;
 using Autofac;
 using Autofac.Features.OwnedInstances;
@@ -14,13 +15,13 @@ namespace Flower.Autofac.Tests
         {
             // Arrange
             var containerBuilder = new ContainerBuilder();
-            containerBuilder.RegisterType<TestWorker>();
+            containerBuilder.RegisterType<TestWorkerIntToStringDisposable>();
             var container = containerBuilder.Build();
             var workRegistry = new WorkRegistry();
             var trigger = new Subject<int>();
-            var resolve = container.Resolve<Func<Owned<TestWorker>>>();
+            var resolve = container.Resolve<Func<Owned<TestWorkerIntToStringDisposable>>>();
             var work = workRegistry.Register(trigger, resolve.Scope());
-            IExecutableFuncWork<int, int> executedWork = null;
+            IExecutableFuncWork<int, string> executedWork = null;
             work.Executed.Subscribe(w => executedWork = w);
 
             // Act
@@ -28,18 +29,59 @@ namespace Flower.Autofac.Tests
 
             // Assert
             Assert.NotNull(executedWork);
-            Assert.Equal(3+1, executedWork.Output);
-            Assert.True((executedWork.WorkerScope.Worker as TestWorker).IsDisposed);
+            Assert.Equal(3.ToString(CultureInfo.InvariantCulture), executedWork.Output);
+            Assert.True((executedWork.WorkerScope.Worker as TestWorkerIntToStringDisposable).IsDisposed);
+        }
+
+        [Fact]
+        public void OwnedInstancesCanBePiped()
+        {
+            // Arrange
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterType<TestWorkerIntToStringDisposable>();
+            containerBuilder.RegisterType<TestWorkerStringToIntDisposable>();
+            var container = containerBuilder.Build();
+            var workRegistry = new WorkRegistry();
+            var trigger = new Subject<int>();
+            var intToString = container.Resolve<Func<Owned<TestWorkerIntToStringDisposable>>>();
+            var stringToInt = container.Resolve<Func<Owned<TestWorkerStringToIntDisposable>>>();
+            var work1 = workRegistry.Register(trigger, intToString.Scope());
+            var work2 = work1.Pipe(stringToInt.Scope());
+            IExecutableFuncWork<string, int> executedWork = null;
+            work2.Executed.Subscribe(w => executedWork = w);
+
+            // Act
+            trigger.OnNext(3);
+
+            // Assert
+            Assert.NotNull(executedWork);
+            Assert.Equal(3, executedWork.Output);
+            Assert.True((executedWork.WorkerScope.Worker as TestWorkerStringToIntDisposable).IsDisposed);
         }
     }
 
-    public sealed class TestWorker : IWorker<int, int>, IDisposable
+    public sealed class TestWorkerIntToStringDisposable : IWorker<int, string>, IDisposable
     {
         public bool IsDisposed { get; private set; }
 
-        public int Execute(int input)
+        public string Execute(int input)
         {
-            return input + 1;
+            return input.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public void Dispose()
+        {
+            IsDisposed = true;
+        }
+    }
+
+    public sealed class TestWorkerStringToIntDisposable : IWorker<string, int>, IDisposable
+    {
+        public bool IsDisposed { get; private set; }
+
+        public int Execute(string input)
+        {
+            return int.Parse(input, CultureInfo.InvariantCulture);
         }
 
         public void Dispose()
