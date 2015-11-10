3a06c64644ba08cd2ea9ece02726a74ac957ed98
Jussi Mattila <jussi.mattila@combinostics.com>
2015-11-10 13:30:55 +0200

    Add trigger interface and base class.

---
+++ Flower/Triggers/TriggerBase.cs
@@ -0,0 +1,29 @@
+﻿namespace Flower.Triggers
+{
+    using System;
+    using System.Reactive.Linq;
+
+    public class TriggerBase<TInput> : ITrigger<TInput>
+    {
+        private event Action<TInput> Triggered;
+        private readonly IObservable<TInput> observable;
+
+        public TriggerBase()
+        {
+            observable = Observable.FromEvent<TInput>(
+                e => Triggered += e,
+                e => Triggered -= e);
+        }  
+
+        public IDisposable Subscribe(IObserver<TInput> observer)
+        {
+            return observable.Subscribe(observer);
+        }
+
+        public void Trigger(TInput input)
+        {
+            var handler = Triggered;
+            handler?.Invoke(input);
+        }
+    }
+}
