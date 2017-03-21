15a683adde5ce0edcec82f5ab349e3e32ce06479
Jussi Mattila <jussi.mattila@combinostics.com>
2017-03-21 10:46:44 +0200

    Update nugets

---
+++ Flower.Autofac/ComponentContextExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿ // ReSharper disable once CheckNamespace Extension methods can be in original namespace
+namespace Autofac
+{
+    using System;
+    using Features.OwnedInstances;
+    using Flower;
+
+    public static class ComponentContextExtensionMethods
+    {
+        public static Func<IScope<TWorker>> ResolveFactory<TWorker>(this IComponentContext componentContext)
+        {
+            return componentContext.Resolve<Func<Owned<TWorker>>>().Scope();
+        }
+    }
+}
